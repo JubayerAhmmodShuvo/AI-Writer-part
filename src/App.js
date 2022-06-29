1ba07ef60d8b350570c660e569fda373d9bf141e
Jubayer Ahmmod <jubayer15-10807@diu.edu.bd>
2022-06-29 18:12:32 +0600

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Ads from './Pages/ADS/Ads';
+import AiWriter from './Pages/AiWriter/AiWriter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+      <AiWriter />
+      < Routes>
+      <Route path='/home' element={<Home />} ></Route>
+      <Route path='/ads' element={<Ads />} ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/ADS/Ads.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import styles from './Ads.module.css';
+const Ads = () => {
+  return (
+    <div className="mt-5" >
+      <h1 className='mb-5 text-center '>Data Driven CopyWriting </h1>
+
+      <section className='' >
+        <div className = { styles.container } >
+           <div class={styles.containerr}> <a name="facebook-ads" href="#modal1" data-toggle="modal" data-target="#exampleModal">
+                
+                        <div><i class="fab fa-facebook"></i> 
+                            
+                        <h4 class="d-inline card-title" style={{}}> Facebook Ads</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Generate Exceptionally Effective Copy For Your Facebook Ads</p>
+                        </div>   
+          </a></div>
+           <div class={styles.containerr} ><a href="#modal9" data-toggle="modal" data-target="#modal9">
+                        <div ><i class="fab fa-google" style={{color:'red'}}></i>
+                            <h4 class="d-inline card-title" style={{}}> Google Search Ads</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Create high-converting descriptions and headlines for your Google Search Ads</p>
+                        </div>
+          </a></div>
+           <div class={styles.containerr} ><a href="#modal10" data-toggle="modal" data-target="#modal10">
+                        <div ><i class="fab fa-twitter"></i>
+                            <h4 class="d-inline card-title" style={{}}>Twitter Ads</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Write scroll-stopping copy for your Twitter Ads.</p>
+                        </div>
+          </a></div>
+          
+                    
+       </div>
+      </section>
+      
+   
+      <section>
+        <div className={styles.container}>
+          <div class={styles.containerr} ><a href="#modal2" data-toggle="modal" data-target="#modal2">
+                        <div ><i class="fab fa-facebook"></i>
+                            <h4 class="d-inline card-title " style={{}}> Facebook Post</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`} >Creating Engaging Copy For Your Facebook Posts To Maximize Organic Reach.</p>
+                        </div>
+        </a></div>
+         <div class={styles.containerr} ><a href="#modal11" data-toggle="modal" data-target="#modal11">
+                        <div ><i class="fab fa-instagram" style={{color: 'pink'}}></i>
+                            <h4 class="d-inline card-title" style={{}}>Instagram Caption</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Generate Catchy Captions For Your Instagram Posts.</p>
+
+                    </div></a></div>
+                    <div class={styles.containerr} ><a href="#modal12" data-toggle="modal" data-target="#modal12">
+                    <div ><i class="fab fa-linkedin"></i>
+                            <h4 class="d-inline card-title" style={{}}>Linkedin Post</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Cut Through The Noise By Generating Compelling Copy For Your LinkedIn posts.</p>
+          </div></a></div></div>
+        </section>
+      <section>
+
+    <div className={styles.container}>
+            <div class={styles.containerr}><a href="#modal3" data-toggle="modal" data-target="#modal3">
+                    <div ><i class="fab fa-twitter" style={{color:'blue'}}></i>
+                            <h4 class="d-inline card-title" style={{}}>Tweet</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Generate Witty Tweets To Keep Your Folowers Engaged.</p>
+                        </div>
+                    </a></div>
+            <div class={styles.containerr} ><a href="#modal4" data-toggle="modal" data-target="#modal4">
+                        <div ><i class="fab fa-pinterest" style={{color:'red'}}></i>
+                            <h4 class="d-inline card-title" style={{}}>Pinterest Pin</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Generate stellar descriptions to step up your Pinterest game.</p>
+                        </div>
+                    </a></div>
+             <div class={styles.containerr} ><a href="#modal5" data-toggle="modal" data-target="#modal5">
+                        <div ><i class="fab fa-youtube" style={{color:'red'}}></i>
+                            <h4 class="d-inline card-title" style={{}}>Youtube Description</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Write smart Youtube titles and descriptions to help viewers find your videos</p>
+                        </div>
+                    </a></div></div>
+      </section>
+      
+       <section>
+                   
+                    <div className={styles.container}>
+            <div class={styles.containerr}><a href="#modal6" data-toggle="modal" data-target="#modal6">
+                        <div ><i class="far fa-envelope-open" style={{color: 'black'}}></i>
+                            <h4 class="d-inline card-title" style={{fontSize: '16px'}}>Cold Email</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Create Powerful, Convincing Emails To Attract Potential Customers To Your Product or Service</p>
+                        </div>
+                   </a></div>
+                   <div class={styles.containerr}><a href="#modal7" data-toggle="modal" data-target="#modal7">
+                   <div ><i class="far fa-envelope-open" style={{color: 'black'}}></i>
+                            <h4 class="d-inline card-title" style={{}}>Promotional Email</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Have something amazing for your customers? Let them know! Write an awesome email about a new product or an exciting promotion.</p>
+                    </div></a></div></div>
+                </section>
+                <section>
+                 
+                    <div div className = {styles.container} >
+            <div class={styles.containerr}  ><a href="#modal8" data-toggle="modal" data-target="#modal8">
+                        <div ><i class="far fa-comment-alt" style={{color: 'black'}}></i>
+                            <h4 class="d-inline card-title" >Promotional SMS</h4>
+                            <p class={`${styles.jss365} ${styles.jss918} ${styles.jss367}`}>Generate Text Messages That Actually Sell.</p>
+                    </div></a></div>
+
+    </div>
+</section>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="modal" id="modal9">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h2>Google Search Ad Project</h2>
+                        </div>
+                        <div class="modal-body">
+                            <label>Brand/Product Name</label>
+                              <input class="form-control" type="text"/>
+                            <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Product description</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                              </div>
+                              <label for="exampleFormControlSelect1">Industry</label>
+                              <select class="form-control form-control sm" id="exampleFormControlSelect1">
+                                <option selected>Choose..</option>
+                                <option>Fashion</option>
+                                <option>Food</option>
+                                <option>Wellness</option>
+                                <option>Technology</option>
+                                <option>Furniture</option>
+                                <option>Creativity</option>
+                              </select>
+                        </div>
+                        <div class="modal-footer">
+                            <input class="btn btn-default" data-dismiss="modal" value="Cancel" />
+                            <input class="btn btn-primary"  type="submit" onclick="location.href='googleads/';" value="Create Project" />
+                            
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+         
+
+        </div>
+    </div>
+</div>
+
+          
+</div>
+
+
+       
+    
+  );
+};
+
+export default Ads;
