67bd7b572281389074d3511338c56f34bdd82dc0
rashi2911 <107244393+rashi2911@users.noreply.github.com>
2022-07-01 17:22:38 +0530

    Update Headline.js

--- src/Pages/Shared/Headline.js
+++ src/Pages/Shared/Headline.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import styles from './Headline.module.css';
 const Headline = () => {
+
   return (
-    <div>
-      <h1>I am headline</h1>
-    </div>
+    <div class="container" id="display1">
+            <p id="ele">Template Types:</p>
+            <button id="ele1" className={styles.btn1} onclick="#">Default</button>
+               <button id="ele2" className={styles.btn} onclick="#">Playful</button>
+               <button id="ele3" className={styles.btn} onclick="#">Conversational</button>
+               <button id="ele4" className={styles.btn} onclick="#">List</button>
+           
+               <br/><br/><p>Your Custom Templates</p>
+               <button  id="ele5" className={styles.btn} style={{background:'white', border:'1px solid black'}}><a href="#modal2" data-toggle="modal" data-target="#modal2"><span>+</span> Add New</a></button>
+               <br/><br/><br/><br/><br/><br/><br/><br/>
+               <div style={{border: "1px solid black", width:'40vw'}}></div>
+               <div class="container" id="display2">
+            <button id="btn1" className="btn btn-primary" onclick="generate()">Generate More</button> <nbsp />
+            <label for="myList"></label>
+            <select id = "myList" > 
+                <br/><br/><option value="1"> 3</option>  
+                <option value="2">5 </option>  
+                <option value="3"> 10 </option>  
+                 
+                </select> <nbsp />
+            <button  style={{background:"white", border:"1px solid gray"}}><a href="#modal1" data-toggle="modal" data-target="#modal1">Brief</a></button>
+               </div>
+               </div>
   );
 };
 
