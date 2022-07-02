fd0c49e77d0c4dd3acda0bed6b42a197a2870f83
rashi2911 <107244393+rashi2911@users.noreply.github.com>
2022-07-02 19:11:41 +0530

    Update Headline.js

--- src/Pages/Shared/Headline.js
+++ src/Pages/Shared/Headline.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Headline.module.css';
-const Headline = () => {
+import PowerMode from './PowerMode';
+import ReWrite from './ReWrite';
+import Templates from './Templates';
 
+const Headline = () => {
+  const [active,setActive]=useState("first");
   return (
-    <div class="container" id="display1">
-            <p id="ele">Template Types:</p>
+
+    <div className = { styles.headline}>
+
+<nav>
+
+ < ul className='d-flex justify-content-between px-4 ' >
+   <button className="btn btn-outline-primary mt-4 px-3 py-2 " onClick={()=>setActive("first")} >Template              </button>
+   <button  className = "btn btn-outline-primary mt-4 px-3 py-2" onClick = { () => setActive("second")} > Rewrite</button>
+   <button  className = "btn btn-outline-primary mt-4 px-3 py-2" onClick = {() => setActive("third")} > Power Mode </button>
+{/* < li className = 'mt-4' > <NavLink className={({isActive}) => isActive ? 'text-white bg-primary p-2 rounded':'text-black' } to = "/twitterads/primary/tada" > Templates </NavLink></li >
+< li className = 'mt-4' > <NavLink className={({isActive}) => isActive ? 'text-white bg-primary p-2 rounded':'text-danger' }   to = "/twitterads" > Rewrite </NavLink></li >
+< li className = 'mt-4' > <NavLink className={({isActive}) => isActive ? 'text-white bg-primary p-2 rounded':'text-black' }  to = "/twitterads/headline" > PowerMode </NavLink></li >
+*/}
+</ul>
+
+</nav>
+<div className="">
+              {active=== "first" && <Templates />}
+            {active=== "second" && <ReWrite />}
+            {active=== "third" && <PowerMode />}
+
+          </div>
+          <div class="container" id="display1">
+           <p id="ele">Template Types:</p>
             <button id="ele1" className="btn btn-sm btn-primary" onclick="#">Default</button> <nbsp />
                <button id="ele2" className="btn btn-sm btn-secondary" onclick="#">Playful</button> <nbsp />
                <button id="ele3" className="btn btn-sm btn-secondary" onclick="#">Conversational</button> <nbsp />
@@ -24,6 +50,7 @@ const Headline = () => {
                  
                 </select> <nbsp />
             <button  style={{background:"white", border:"1px solid gray"}}><a href="#modal1" data-toggle="modal" data-target="#modal1">Brief</a></button>
+               </div>
                </div>
                </div>
   );
