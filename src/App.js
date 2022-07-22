6038be97503a15a8d778560f3fed8acda102c567
rashi2911 <rashi29.agarwal@gmail.com>
2022-07-22 15:35:43 +0530

    changes 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from './Pages/Home/Home';
-import Editor from './Pages/Home/editor';
-import Ads from './Pages/ADS/Ads';
-import Document from './Pages/ADS/document';
-import AiWriter from './Pages/AiWriter/AiWriter';
-import TwitterAds from './Pages/ADS/TwitterAds';
-import Save from './Pages/Shared/Save';
-import Primary from './Pages/Shared/Primary';
-import Headline from './Pages/Shared/Headline';
-
+import Home from "./Pages/Home/Home";
+import Editor from "./Pages/Home/editor";
+import Ads from "./Pages/ADS/Ads";
+import Document from "./Pages/ADS/document";
+import AiWriter from "./Pages/AiWriter/AiWriter";
+import TwitterAds from "./Pages/ADS/TwitterAds";
+import FacebookAds from "./Pages/ADS/FacebookAds";
+import GoogleAds from "./Pages/ADS/GoogleAds";
+import Save from "./Pages/Shared/Save";
+import Primary from "./Pages/Shared/Primary";
+import Headline from "./Pages/Shared/Headline";
 
 function App() {
   return (
-    <div  >
+    <div>
       <AiWriter />
-      < Routes>
-      <Route path='/home' element={<Home />} ></Route>
-      <Route path='/editor' element={<Editor />} ></Route>
-      <Route path='/document' element={<Document />} ></Route>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/editor" element={<Editor />}></Route>
+        <Route path="/document" element={<Document />}></Route>
 
-      <Route path='/ads' element={<Ads />} ></Route>
-        <Route path='twitterads' element={<TwitterAds />} >
-          <Route path="save" element={<Save />}/>
-          <Route index element={<Primary />}/>
-          <Route path="primary" element={<Primary />}/>
-          <Route path="headline" element={<Headline />}/>
-         
-          
+        <Route path="/ads" element={<Ads />}></Route>
+        <Route path="twitterads" element={<TwitterAds />}>
+          <Route path="save" element={<Save />} />
+          <Route index element={<Primary />} />
+          <Route path="primary" element={<Primary />} />
+          <Route path="headline" element={<Headline />} />
         </Route>
-      
-      
+        <Route path="facebookads" element={<FacebookAds />}>
+          <Route path="save" element={<Save />} />
+          <Route index element={<Primary />} />
+          <Route path="primary" element={<Primary />} />
+          <Route path="headline" element={<Headline />} />
+        </Route>
+        <Route path="googleads" element={<GoogleAds />}>
+          <Route path="save" element={<Save />} />
+          <Route index element={<Primary />} />
+          <Route path="primary" element={<Primary />} />
+          <Route path="headline" element={<Headline />} />
+        </Route>
+
       </Routes>
     </div>
   );
---
+++ src/Pages/ADS/GoogleAds.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+
+import { NavLink, Link, Outlet, useNavigate } from "react-router-dom";
+import styles from "./TwitterAds.module.css";
+
+import Primary from "../Shared/Primary";
+import Lists from "../Shared/Lists";
+import WhiteBoard from "../Shared/WhiteBoard";
+
+const GoogleAds = () => {
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  // const [show,setShow]=useState(true);
+  // const List = () => {
+  //   isHidden: true;
+  // };
+
+  return (
+    <>
+      {/* <Navbar bg="light" variant="dark">
+    <Container>
+    <Nav className="me-auto">
+      <Nav.Link href="#"><h6 className={styles.linkText1}>Template  </h6> </Nav.Link>
+    <Nav.Link href="#features"><h6 className={styles.linkText}>   Rewrite </h6> </Nav.Link>
+      <Nav.Link href="#pricing"><h6 className={styles.linkText}>Power Mode </h6> </Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar> */}
+
+      <div className="row">
+        <div className="col-2">
+          <div className={styles.sidebar}>
+            <nav>
+              <ul className="py-5">
+                <li className="py-4">
+                  {" "}
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-white  bg-primary p-2 rounded"
+                        : "text-black"
+                    }
+                    to="/googleads/save"
+                  >
+                    {" "}
+                    Saved{" "}
+                  </NavLink>
+                </li>
+                <li className="py-4">
+                  {" "}
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-white  bg-primary p-2 rounded"
+                        : "text-black"
+                    }
+                    to="/googleads/primary"
+                  >
+                    {" "}
+                    Primary Text
+                  </NavLink>
+                </li>
+                <li className="py-4">
+                  {" "}
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-white  bg-primary p-2 rounded"
+                        : "text-black"
+                    }
+                    to="/facebookads/headline"
+                  >
+                    {" "}
+                    Headline{" "}
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.v1}></div>
+        </div>
+        <div className="col-4">
+          <Outlet />
+        </div>
+        <div className="col-6">
+          <ul className="d-flex   px-5 ">
+            <button
+              className="btn me-3 btn-outline-primary mt-4 px-4 py-2 float-right"
+              onClick={() => setActive("second")}
+            >
+              {" "}
+              List
+            </button>
+            <button
+              id="whiteboard"
+              className="btn btn-outline-primary mt-4 px-4 py-2 float-right"
+              onClick={() => setActive("third")}
+            >
+              {" "}
+              WhiteBoard{" "}
+            </button>
+            {/* < li className = 'mt-4' > <NavLink className={({isActive}) => isActive ? 'text-white bg-primary p-2 rounded':'text-black' } to = "/twitterads/primary/tada" > Templates </NavLink></li >
+          < li className = 'mt-4' > <NavLink className={({isActive}) => isActive ? 'text-white bg-primary p-2 rounded':'text-danger' }   to = "/twitterads" > Rewrite </NavLink></li >
+          < li className = 'mt-4' > <NavLink className={({isActive}) => isActive ? 'text-white bg-primary p-2 rounded':'text-black' }  to = "/twitterads/headline" > PowerMode </NavLink></li >
+          */}
+          </ul>
+          <div className="">
+            {active === "second" && <Lists />}
+            {active === "third" && <WhiteBoard />}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GoogleAds;
